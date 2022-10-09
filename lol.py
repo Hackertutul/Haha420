219228785a60174724d2aee636e7aed507870b9e
Hackertutul <103777747+Hackertutul@users.noreply.github.com>
2022-10-10 01:20:44 +0600

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,31 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+
+        os.system('xdg-open https://www.facebook.com/FB.KING.MAHIN.NAME.TOH.SONSO')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+
+ 
+        
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from lol import main1
+ 
+ 
+        main1()
+        
